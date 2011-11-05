d97424211df8d21aba6bedb0bbd782c381e90b24
Martin Frost <frost@ceri.se>
2011-11-05 03:24:55 +0100

    Added default focus to newly created input fields.

--- public/mall.js
+++ public/mall.js
@@ -4,12 +4,16 @@ $(document).ready(function () {
     $(container).append(template.replace(/TIMESTAMP/g, timestamp));
   }
 
-  $('#add_author').click(function () {
+  $('#add_author').click(function (event) {
     appendItem("#authors",'<li><input type="text" name="authors[TIMESTAMP]" id="authors_TIMESTAMP"></li>');
+    $("#authors li:last input").focus();
+    return false;
   });
 
-  $('#add_item').click(function () {
-    appendItem("#items", '<li><b>att</b> <textarea name="items[TIMESTAMP]" id="authors_TIMESTAMP" placeholder="Ytterligare någon vettig sak..."></textarea></li>');
+  $('#add_item').click(function (event) {
+    appendItem("#items", '<li><b>att</b> <textarea name="items[TIMESTAMP]" id="items_TIMESTAMP" placeholder="Ytterligare någon vettig sak..."></textarea></li>');
+    $("#items li:last textarea").focus();
+    return false;
   });
 
   $('textarea').ata();
